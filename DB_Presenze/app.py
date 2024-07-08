e760a62a91adb61eee6f6b1ab2852e28a7af5180
davideDD19 <davide.de.luca019@gmail.com>
2024-07-08 10:34:02 +0200

    Add files via upload

--- DB_Presenze/app.py
+++ DB_Presenze/app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request, redirect 
-from urllib.parse import quote as url_quote
+from flask import Flask, render_template, request, redirect, url_for
 from datetime import datetime
 
 app = Flask(__name__)
@@ -8,10 +7,25 @@ presenze = []
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        nome = request.form['nome']
-        data = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        presenze.append({'nome': nome, 'data': data})
-        return redirect(url_quote('index'))
+        if 'reset' in request.form:
+            presenze.clear()
+        else:
+            nome = request.form['nome']
+            now = datetime.now().strftime("%H:%M:%S")  # Ottieni l'orario attuale
+            if 'submit_entrata' in request.form:
+                data_entrata = request.form['data_entrata']
+                presenze.append({
+                    'nome': nome,
+                    'data_entrata': f"{data_entrata} {now}",  # Aggiungi l'orario alla data di entrata
+                    'data_uscita': None
+                })
+            elif 'submit_uscita' in request.form:
+                data_uscita = request.form['data_uscita']
+                for presenza in presenze:
+                    if presenza['nome'] == nome and presenza['data_uscita'] is None:
+                        presenza['data_uscita'] = f"{data_uscita} {now}"  # Aggiungi l'orario alla data di uscita
+                        break
+        return redirect(url_for('index'))
     return render_template('index.html', presenze=presenze)
 
 if __name__ == '__main__':
